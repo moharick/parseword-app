2a1a6ce578627c1be54d8b147529fbd6c2285dca
moharick <moharick@gmail.com>
2019-09-05 14:50:03 +0300

    Add function that generates new users.

--- parseword-app/parseword.py
+++ parseword-app/parseword.py
@@ -4,4 +4,13 @@ import string
 import emoji
 from user import User
 from credentials import Credential
+userList = []
 
+
+def __init__(self, name, username, password):
+    """
+    Class that generates new users.
+    """
+    self.username = username
+    self.name = name
+    self.password = password
