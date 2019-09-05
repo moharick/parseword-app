501b010dca1d18f66d9a66d91b2ae65c1aca95b6
moharick <moharick@gmail.com>
2019-09-05 14:51:41 +0300

    Add function that deletes credentials.

--- parseword-app/parseword.py
+++ parseword-app/parseword.py
@@ -14,3 +14,10 @@ def __init__(self, name, username, password):
     self.username = username
     self.name = name
     self.password = password
+
+
+def del_app(credentials):
+    '''
+    Function to delete a credential
+    '''
+    credentials.del_app()
